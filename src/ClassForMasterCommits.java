e3124c3d891afdd0c9f306dae5dbeae76b5d36c2
Pavel Savitskiy <Pavel_Savitskiy@epam.com>
2020-07-03 22:02:24 +0600

    Resolve all the conflicts

--- src/ClassForMasterCommits.java
+++ src/ClassForMasterCommits.java
@@ -1,11 +1,9 @@
 public class ClassForMasterCommits {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Preparing for the rebase");
-=======
+
         String stringForCherryPicking = "Just a string for cherry-picking";
 
         System.out.println("");
->>>>>>> e0e555b... Add a useful string variable
+
     }
 }

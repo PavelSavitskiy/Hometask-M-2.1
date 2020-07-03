02d002050314f3825039e6aa5632afbbb63db3b6
Pavel Savitskiy <Pavel_Savitskiy@epam.com>
2020-07-03 22:16:47 +0600

    Revert the last commit in develop branch

--- src/ClassForMasterCommits.java
+++ src/ClassForMasterCommits.java
@@ -1,7 +1,6 @@
 public class ClassForMasterCommits {
     public static void main(String[] args) {
         String stringForCherryPicking = "Just a string for cherry-picking";
-        String notUsefulString;
 
         System.out.println("");
     }

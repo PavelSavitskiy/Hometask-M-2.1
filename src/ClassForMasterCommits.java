23c5816cefb16318221826935054034fb0b31112
Pavel Savitskiy <Pavel_Savitskiy@epam.com>
2020-07-03 21:42:20 +0600

    Add one more string variable

--- src/ClassForMasterCommits.java
+++ src/ClassForMasterCommits.java
@@ -1,6 +1,7 @@
 public class ClassForMasterCommits {
     public static void main(String[] args) {
         String stringForCherryPicking = "Just a string for cherry-picking";
+        String notUsefulString;
 
         System.out.println("");
     }

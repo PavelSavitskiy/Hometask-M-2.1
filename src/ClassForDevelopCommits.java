e9442db84902b868dca67de8f5ab38421bc79acd
Pavel Savitskiy <Pavel_Savitskiy@epam.com>
2020-07-03 21:01:09 +0600

    Prepare develop branch for the rebase

--- src/ClassForDevelopCommits.java
+++ src/ClassForDevelopCommits.java
@@ -1,6 +1,6 @@
 public class ClassForDevelopCommits {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("Preparing for the rebase...");
 
 
     }

316e7fa3d1edf7e8dc8c86d67b1404b65fa12b67
Pavel Savitskiy <Pavel_Savitskiy@epam.com>
2020-07-03 21:32:10 +0600

    Clear the messages copletely

--- src/ClassForMasterCommits.java
+++ src/ClassForMasterCommits.java
@@ -1,5 +1,5 @@
 public class ClassForMasterCommits {
     public static void main(String[] args) {
-        System.out.println("Preparing for the rebase");
+        System.out.println("");
     }
 }

31295f3ebd2847ca98f851b1b16b4475979b9192
Pavel Savitskiy <Pavel_Savitskiy@epam.com>
2020-07-03 20:14:22 +0600

    Change the greeting message

--- src/ClassForMasterCommits.java
+++ src/ClassForMasterCommits.java
@@ -1,5 +1,5 @@
 public class ClassForMasterCommits {
     public static void main(String[] args) {
-        System.out.println("Hello my friend!");
+        System.out.println("Hello my dear friend!");
     }
 }
